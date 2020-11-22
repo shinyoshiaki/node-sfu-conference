2828326cf7c4716809873c5b5185bb19d8ad8a5d
shinyoshiaki <shinyoshiaki2012@gmail.com>
2020-11-22 20:55:01 +0900

    fix storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,9 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
-}
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  babel: async (options) => ({
+    ...options,
+    presets: ["react-app"],
+    plugins: ["react-require"],
+  }),
+};
