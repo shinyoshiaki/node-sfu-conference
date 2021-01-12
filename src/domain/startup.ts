aa0b0ac4f961e79689c4d2aafc0bcc8970c9b737
shinyoshiaki <shinyoshiaki2012@gmail.com>
2021-01-12 19:10:12 +0900

    refactor

--- src/domain/startup.ts
+++ src/domain/startup.ts
@@ -8,11 +8,22 @@ export function useStartup(dispatch: Dispatch, { rtc }: ContextProps) {
   const [lock, setLock] = useState(true);
 
   useAsyncEffect(async () => {
-    const { cam, audio } = await gUM();
+    const media = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true,
+    });
+    const [cam] = media.getVideoTracks();
+    dispatch(setCam({ cam }));
+    const [audio] = media.getAudioTracks();
+    dispatch(setAudio({ audio }));
+
     const rtc = await join();
     rtc.listen(dispatch);
 
-    await publish([cam, audio]);
+    const audioInfo = await rtc.publish({ track: audio });
+    dispatch(setAudioInfo({ info: audioInfo }));
+    const camInfo = await rtc.publish({ track: cam, simulcast: true });
+    dispatch(setCamInfo({ info: camInfo }));
 
     const infos = (await rtc.getMedias()).filter(
       (info) => info.publisherId !== rtc.peerId
@@ -21,19 +32,6 @@ export function useStartup(dispatch: Dispatch, { rtc }: ContextProps) {
 
     setLock(false);
   }, [dispatch]);
-
-  async function gUM() {
-    const media = await navigator.mediaDevices.getUserMedia({
-      video: true,
-      audio: true,
-    });
-    const cam = media.getVideoTracks()[0];
-    dispatch(setCam({ cam }));
-    const audio = media.getAudioTracks()[0];
-    dispatch(setAudio({ audio }));
-
-    return { cam, audio };
-  }
 
   async function join() {
     const params = new URLSearchParams(window.location.hash.split("#")[1]);
@@ -47,13 +45,6 @@ export function useStartup(dispatch: Dispatch, { rtc }: ContextProps) {
 
     await rtc.apiJoin();
     return rtc;
-  }
-
-  async function publish([video, audio]: MediaStreamTrack[]) {
-    const audioInfo = await rtc.publish({ track: audio });
-    dispatch(setAudioInfo({ info: audioInfo }));
-    const camInfo = await rtc.publish({ track: video, simulcast: true });
-    dispatch(setCamInfo({ info: camInfo }));
   }
 
   return lock;
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,14 +1,10 @@
-import styled from "@emotion/styled";
-import { Divider } from "antd";
 import Layout from "antd/lib/layout/layout";
 import { FC, useContext } from "react";
 import { useDispatch } from "react-redux";
 import LeftBottom from "../components/left-bottom";
 import RightMenu from "../components/right-menu";
 import LocalStream from "../containers/local-stream";
-import { LocalMedia } from "../containers/localMedia";
 import RemoteStreams from "../containers/remote-streams";
-import { RemoteMediaList } from "../containers/remoteMediaList";
 import { Context } from "../context/context";
 import { useStartup } from "../domain/startup";
 
@@ -29,23 +25,3 @@ export const App: FC = () => {
     </Layout>
   );
 };
-
-const Container = styled.div`
-  position: relative;
-  height: 98vh;
-`;
-
-const Users = styled.div`
-  padding: 10px;
-  height: 100%;
-  width: 250px;
-  @media (max-width: 420px) {
-    width: 100%;
-  }
-  display: flex;
-  flex-direction: column;
-`;
-
-const StyledRemoteMediaList = styled(RemoteMediaList)`
-  flex-grow: 1;
-`;
